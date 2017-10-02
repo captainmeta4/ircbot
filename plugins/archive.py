a1c46df5bd7fb9cc98a9c2ab250f20c0e5d0e995
captainmeta4 <admin@MacBook-Air.local>
2017-10-02 15:16:12 -0400

    add archive command

---
+++ plugins/archive.py
@@ -0,0 +1,38 @@
+from __main__ import Plugin
+import requests
+
+user_agent="archive plugin for /u/captainmeta4's IRC interface"
+
+class Main(Plugin):
+
+    def exe(self, message):
+
+        url=self.args[1]
+
+        #apis
+        check='http://archive.org/wayback/available?url={}'
+        save = 'https://web.archive.org/save/{}'
+        headers={'User-Agent':user_agent}
+
+        
+
+        #check to see if it is already archived
+
+        x=requests.get(check.format(url), headers=headers)
+        j=x.json()
+        if 'closest' in j['archived_snapshots']:
+            yield j['archived_snapshots']['closest']['url']
+            return
+
+        #archive it
+
+        x=requests.get(save.format(url), headers=headers)
+        if x.status_code != 200:
+            yield "There was an error archiving your url"
+            return
+
+        yield "I have initiated. Try again in a minute or two to see if there's a saved archive page."
+
+        
+
+        
