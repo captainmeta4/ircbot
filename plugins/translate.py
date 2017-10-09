03e7d617ac734525ea49c7c55fa9a8bb794e45f6
captainmeta4 <admin@MacBook-Air.local>
2017-10-09 14:54:19 -0400

    add translate

---
+++ plugins/translate.py
@@ -0,0 +1,46 @@
+from __main__ import Plugin
+import requests
+import os
+
+class Main(Plugin):
+
+    def on_start(self):
+
+        self.key=os.environ.get('yandex_key')
+        self.headers={"User-Agent":"Translation Module for captainmeta4 IRC Interface"}
+        
+        #get supported languages
+        url='https://translate.yandex.net/api/v1.5/tr.json/getLangs'
+        params = {'key': self.key,
+                  'ui':'en'}
+        x=requests.get(url,params=params, headers=self.headers)
+        self.langs=x.content.decode('utf-8')
+
+    def helptext(self):
+
+        yield "$translate <language> <text>"
+        yield "Translate <text> to <language> using Yandex translation engine."
+
+        
+    def exe(self, message):
+
+        #valid languages
+
+        if self.args[1] not in self.langs['langs']:
+            yield 'Target language not specified.'
+            return
+
+        #get text to translate
+        text=message.body.split(maxsplit=2)
+
+        #Assemble params for translation
+        params={'key':self.key,
+                'text':text,
+                'lang':self.args[1],
+                'format':plain}
+        url='https://translate.yandex.net/api/v1.5/tr.json/translate'
+
+        #hit translation api
+        response = requests.get(url,params=params,headers=self.headers)
+
+        yield response.json()['text'][0]
